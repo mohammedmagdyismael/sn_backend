a8ff603b6c337118f8338490d12b73d98f5a29da
Mohammed Magdy Ismael <m.magdy.isl@gmail.com>
2020-04-06 05:25:33 +0200

    Add google anayltics configs

---
+++ config/google-analytics.js
@@ -0,0 +1,16 @@
+/**
+ * Add Firebase to your project https://firebase.google.com/docs/web/setup
+ */
+//DevConnector
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUr-_t5Gg483tDElpsywrdvJV3UoZKeHY",
+    authDomain: "devconnector-c92b3.firebaseapp.com",
+    databaseURL: "https://devconnector-c92b3.firebaseio.com",
+    projectId: "devconnector-c92b3",
+    storageBucket: "devconnector-c92b3.appspot.com",
+    messagingSenderId: "874289896805",
+    appId: "1:874289896805:web:f1bb552833d47ad4"
+  };
+
+
